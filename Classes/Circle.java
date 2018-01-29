e3298dbec9c22d5af9715cb4deb687fad225410d
GabeYA <gabe.bertoluzzi@gmail.com>
2018-01-29 15:18:19 -0600

    Complete Circle class work

--- Classes/Circle.java
+++ Classes/Circle.java
@@ -1,21 +1,28 @@
 public class Circle {
-    // Getting work started.  
-    // Java is up and running on computer.
-    // Success on first commit as well
+   
+    // Private variables
     private double radius;
     private String color;
 
+
+    
     // the default constructor with no argument
     // It sets the radius and color to their default values
     public Circle() {
-        radius = 1.0;
-        color = "red";
+        this.radius = 1.0;
+        this.color = "red";
     }
 
     // 2nd constructor with given radius, but color default
-    public Circle(double r) {
-        radius = r;
-        color = "red";
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = "red";
+    }
+
+    // 3rd constructor with given radius and color
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
     }
 
     // A public method for retrieving the radius
@@ -28,4 +35,25 @@ public class Circle {
         return radius*radius*Math.PI;
     }
 
+    // A public method for retreiving the color
+    public String getColor() {
+        return color;
+    }
+
+    // Setter for instance variable radius
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    // Setter for instance variable color
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    // Return a description of this instance in the form of 
+    // Circle[radius=r,color=c]
+    public String toString() {
+        return "Circle[radius=" + radius + ",color=" + color + "]";
+    }
 }
